d2d46c2dab6ce2e4fbb40cdc301ae8c7f4f6e831
Jessy Mangat <jessymangat22@hotmail.com>
2019-05-26 10:46:00 -0400

    returns status code

--- router.js
+++ router.js
@@ -10,5 +10,16 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
+router.post('/register', (res) => {
+  res.statusCode = 201;
+});
+
+router.post('/login', (res) => {
+  res.statusCode = 202;
+});
+
+router.get('/product', (res) => {
+  res.statusCode = 200;
+});
 
 module.exports = router;
