70efe8fe2fa88374460c3f8004f1639a83509bc9
JessyMangat <jessymangat22@hotmail.com>
2019-05-26 11:51:36 -0400

    update and delete by id

--- router.js
+++ router.js
@@ -10,16 +10,33 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-router.post('/register', (res) => {
+router.post('/register', (req, res, next) => {
   res.statusCode = 201;
 });
 
-router.post('/login', (res) => {
+router.post('/login', (req, res, next) => {
   res.statusCode = 202;
 });
 
-router.get('/product', (res) => {
+router.get('/product', (req, res, next) => {
   res.statusCode = 200;
 });
+
+router.get('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.post('/item', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.delete('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
+router.patch('/item/:id', (req, res, next) => {
+  res.statusCode = 200;
+});
+
 
 module.exports = router;
